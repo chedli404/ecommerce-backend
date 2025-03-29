15245eb2dfd67094834b9441861666cd8344aea1
karim chedli frini <chedlifrini@gmail.com>
2025-03-29 19:24:41 +0100

    Update authRouter.js

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import User from '../models/user.js'
+import {OAuth2Client} from 'google-auth-library'
 
 const router = express.Router()
+const client = new OAuth2Client('414739204309-6tg5ljlcn2d0oofgb51sulcrere6u44h.apps.googleusercontent.com')
 
 router.post('/login', async (req, res) => {
     try {
@@ -22,5 +24,57 @@ router.post('/register', async (req, res) => {
         res.status(500).json({message: err.message})
     }
 })
+
+router.post('/google-login', async (req, res) => {
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: req.body.token,
+            audience: '414739204309-6tg5ljlcn2d0oofgb51sulcrere6u44h.apps.googleusercontent.com'
+        });
+        const payload = ticket.getPayload();
+        const user = await User.findOne({email: payload.email});
+        if(!user){
+            const newUser = new User({
+                name: payload.name,
+                email: payload.email,
+                password: payload.sub
+            });
+            await newUser.save();
+            return res.status(201).json(newUser);
+        }
+        res.status(200).json(user);
+    }catch(err){
+        res.status(500).json({message: err.message})
+        console.log('Google login error:', error);
+    }
+})
+
+router.post('register/google', async (req, res) => {
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: req.body.token,
+            audience: '414739204309-6tg5ljlcn2d0oofgb51sulcrere6u44h.apps.googleusercontent.com'
+        });
+        const payload = ticket.getPayload();
+        const user = await User.findOne({email: payload.email});
+        if(!user){
+            const newUser = new User({
+                name: payload.name,
+                email: payload.email,
+                password: payload.sub
+            });
+            await newUser.save();
+            return res.status(201).json(newUser);
+        }
+        res.status(200).json(user);
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+})
+
+
+
+
+
 
 export default router;
