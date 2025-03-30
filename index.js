421670ac2e435c84c1de2eb409abaee70c0c5362
karim chedli frini <chedlifrini@gmail.com>
2025-03-30 01:02:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ mongoose.connect(process.env.MONGO_URI)
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['https://ecommerce-backend-5-irs9.onrender.com'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
